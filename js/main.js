f2044a2eae59c4eea9b2c1462c3f317afaad5bd4
yeomgeunyeong <yyynyeong@gmail.com>
2024-01-19 17:52:06 +0900

    01-19

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+
+    $(".select_language").click(function(){
+        $(this).toggleClass("show")
+    })
+
+
+    //gnb메뉴
+    $(".sub").hide();
+    $(".gnb_menu > li").hover(function(){
+        $(this).find(".sub").stop().slideDown();
+        $(".gnb_menu").stop().animate({height:"80px"}, 500);
+    }, function(){
+        $(this).find(".sub").stop().slideUp();
+        $(".gnb_menu").stop().animate({height:"50px"}, 500);
+    })
+
+
+})//jquery
