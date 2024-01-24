b1b91e6d4100853df0015db5276f4a7232fc86e0
yeomgeunyeong <yyynyeong@gmail.com>
2024-01-24 17:46:36 +0900

    01-24

--- js/main.js
+++ js/main.js
@@ -55,4 +55,40 @@ $(function(){
         arrows:true,
         fade:true,
     })
+
+
+
+
+    //모바일 영역
+    $(".mob_nav_btn").click(function(){
+        $(".mob_nav").addClass("left_move");
+        $(".m_nav_bg").delay(300).fadeIn();
+    })//
+
+    $(".m_nav_bg").click(function(){
+        $(".mob_nav").removeClass("left_move");
+        $(".m_nav_bg").fadeOut();
+    })
+
+
+    $(".m_nav_list > li").hover(function(){
+        $(this).find(".m_sub").stop().slideDown();
+        $(this).find("a").addClass("on")
+    }, function(){
+        $(this).find(".m_sub").stop().slideUp();
+        $(this).find("a").removeClass("on")
+    })
+
+    // $(".m_nav_list > li").click(function(){
+    //     $(this).find("a").addClass("on");
+    //     $(this).find(".m_sub").stop().slideDown();
+    // })
+
+  
+    
+    // $(".m_nav_list > li").click(function(){
+    //     $(".m_sub").stop().slideUp();
+    // });
+
+
 })//jquery
